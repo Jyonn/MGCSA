ce61883209ce8c8e103aa3740bfcfdf5d42f28e9
lqj679ssn <lqj679ssn@qq.com>
2019-11-30 19:08:25 +0800

    v1 MGCSA with evaluator

--- Base/evaluator.py
+++ Base/evaluator.py
@@ -10,7 +10,7 @@ class Evaluator:
         batch_size, candidate_num = logits.get_shape().as_list()
         _, rank = tf.nn.top_k(logits, k=candidate_num)  # [batch_size, candidate_num]
         expand_ans = tf.tile(tf.expand_dims(ans, 1), [1, candidate_num])
-        self.ans_index = tf.argmax(tf.to_int32(tf.equal(rank, expand_ans))) + 1
+        self.ans_index = tf.argmax(tf.to_int32(tf.equal(rank, expand_ans)), axis=1) + 1
 
     def get_mrr(self):
         return tf.reduce_mean(1 / self.ans_index)
--- net.py
+++ net.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
     hp = HyperParams()
     loader = DataLoader(hp, mode='train')
     crazyData = CrazyData(
-        pid='ZFax', ticket='3Wy10a2HuUjiSDRA3jd4NWfwYRZH1lRzdvIMqutec47ELj2WzW3QwKbPkWM0ASSW')
+        pid='cGZH', ticket='WPApHihXtlxL8xo7IEZQ9d2PCxhkOqhb1qJzt0eXYCxRqlNAKmvPwrbgGmQ3aNv1')
 
     tf.reset_default_graph()
 
@@ -166,18 +166,28 @@ if __name__ == '__main__':
             index = 0
             print('epoch', epoch)
             for VGG, C3D, HIS, QUE, ANS, CAN, ANS_ID, turn_nums in loader.get_batch_data():
-                _, mean_loss = sess.run((n.train_operation, n.mean_loss), feed_dict={
-                    n.VGG: VGG,
-                    n.C3D: C3D,
-                    n.HIS: HIS,
-                    n.QUE: QUE,
-                    n.ANS: ANS,
-                    n.CAN: CAN,
-                    n.ANS_ID: ANS_ID,
-                })
+                _, mean_loss, pAt1, pAt5, mrr, meanRank = sess.run(
+                    (n.train_operation, n.mean_loss, n.pAt1, n.pAt5, n.mrr, n.meanRank), feed_dict={
+                        n.VGG: VGG,
+                        n.C3D: C3D,
+                        n.HIS: HIS,
+                        n.QUE: QUE,
+                        n.ANS: ANS,
+                        n.CAN: CAN,
+                        n.ANS_ID: ANS_ID,
+                    })
                 if index % 100 == 0:
-                    print(str(datetime.datetime.now()), 'index', index, 'loss', mean_loss)
-                    crazyData.push([dict(label='loss', value=int(mean_loss*1000))])
+                    print(str(datetime.datetime.now()),
+                          'index:', index,
+                          'loss:', mean_loss,
+                          'p@1:', pAt1,
+                          'p@5:', pAt5,
+                          'mrr:', mrr,
+                          'mr', meanRank)
+                    crazyData.push([
+                        dict(label='loss', value=int(mean_loss * 1000)),
+                        dict(label='accuracy', value=int(pAt1 * 10000))
+                    ])
                 index += 1
             print('total index', index, 'start saving!')
             saver.save(sess, os.path.join(hp.logdir, 'epoch{0}.ckpt'.format(epoch)))
